849a8667a02316f0dc48ebec6a3ec489227d74b6
sohailIlyas1996 <51970582+sohailIlyas1996@users.noreply.github.com>
2024-01-02 23:03:11 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,32 +2,38 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const RegisterModel = require('./models/Register');
+
 const app = express();
 
-
-
+// CORS middleware configuration
 app.use(cors({
-    origin: 'https://merns-seven.vercel.app',
-    methods: ['POST', 'GET'],
-    credentials: true // If you're using credentials in your requests
+    origin: 'https://merns-seven.vercel.app', // Allow requests from this origin
+    methods: ['POST', 'GET'], // Specify allowed HTTP methods
+    credentials: true, // If you're using credentials in your requests
 }));
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority');
 
-app.get("/", (req, res) => {
-    res.json("Hello");
+mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Sample route handling
+app.get('/', (req, res) => {
+    res.json('Hello from server!');
 });
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+
     RegisterModel.findOne({ email: email })
         .then(user => {
             if (user) {
-                res.json("Already have an account");
+                res.json('Already have an account');
             } else {
-                RegisterModel.create({ name: name, email: email, password: password })
+                RegisterModel.create({ name, email, password })
                     .then(result => res.json(result))
                     .catch(err => res.json(err));
             }
@@ -36,5 +42,5 @@ app.post('/register', (req, res) => {
 });
 
 app.listen(3001, () => {
-    console.log("Server is Running");
+    console.log('Server is running on port 3001');
 });
