f64b1ba36b34605ae1377043c3100327158890b8
sohailIlyas1996 <51970582+sohailIlyas1996@users.noreply.github.com>
2024-01-02 22:54:16 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,38 +1,40 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const RegisterModel = require('./models/Register');
 
-const app = express()
-app.use(cors(
-    {
-        origin: ["https://merns-seven.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json())
+const app = express();
 
-mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority')
+// Allow requests from specific origins
+app.use(cors({
+    origin: "https://merns-seven.vercel.app",
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority');
 
 app.get("/", (req, res) => {
     res.json("Hello");
-})
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-})
+});
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("Already have an account");
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => res.json(result))
+                    .catch(err => res.json(err));
+            }
+        })
+        .catch(err => res.json(err));
+});
 
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+    console.log("Server is Running");
+});
