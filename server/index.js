6fb4e4c7298e0498e93a1a0d4ae9c9e9e240899e
sohailIlyas1996 <51970582+sohailIlyas1996@users.noreply.github.com>
2024-01-02 23:12:28 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,41 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const RegisterModel = require('./models/Register');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./models/Register')
+const studentModel = require('./models/student')
 
-const app = express();
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
 
-// CORS middleware configuration
+mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority')
 
-app.use(express.json());
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.post('/all', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
+})
 
 
-mongoose.connect('mongodb+srv://sohaililyas487:admin123@cluster0.biprxao.mongodb.net/Attendance?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
 
-// Sample route handling
-app.get('/', (req, res) => {
-    res.json('Hello from server!');
-});
+router.get('/allstudents',async (req, res) => {
+    try {
+      const allStudents = await studentModel.find({});
+    // Create a new PDF document
+    
+  
 
-app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-
-    RegisterModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                res.json('Already have an account');
-            } else {
-                RegisterModel.create({ name, email, password })
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            }
-        })
-        .catch(err => res.json(err));
-});
+  
+    
+      // res.render('studentData', { students: allStudents}); // Render 'index.ejs' and pass fetched data
+    res.send(allStudents);
+    
+    
+    } catch (err) {
+      console.error('Error fetching student data:', err);
+      res.status(500).send('Error fetching student data');
+    }
+  });
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-});
+    console.log("Server is Running")
+})
